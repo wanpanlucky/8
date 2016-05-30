cb4f462872b66dde8ed56d55aaecc920587d61a3
wanpanlucky <1462607050@qq.com>
2016-05-30 18:53:51 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+var arr=new Array();
+		var Len=collection.length;
+		for(var i=0;i<Len;i++){
+			if(collection[i]%2==0)
+				arr.push(collection[i]);
+		}
+	return arr;
 }
 
 module.exports = collect_all_even;
